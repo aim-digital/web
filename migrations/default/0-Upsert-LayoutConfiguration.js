6bfbd955bd7aa0debe5a80cca11f77a916898f64
Peter C. Romano <pete@vitruvian.tech>
2017-11-28 15:45:53 -0500

    Rename migration

--- migrations/default/0-Upsert-LayoutConfiguration.js
+++ migrations/default/0-Upsert-LayoutConfiguration.js
@@ -2,14 +2,15 @@ import {getModels} from '@machete-platform/core-bundle/lib/Sequelize';
 
 export default class {
   static async up(models, sequelize, DataTypes) {
-    return await getModels().Layout.create({
+    return await getModels().LayoutConfiguration.create({
       theme: '@vitruvian-tech/machete-bundle:default',
-      app: '@machete-platform/core-bundle:App'
+      app: '@machete-platform/core-bundle:App',
+      PageConfigurationId: 1
     });
   }
 
   static async down(models, sequelize, DataTypes) {
-    return await getModels().Layout.destroy({
+    return await getModels().LayoutConfiguration.destroy({
       where: {
         theme: '@vitruvian-tech/machete-bundle:default',
         app: '@machete-platform/core-bundle:App'
