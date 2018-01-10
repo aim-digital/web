9823f4602de69e8023cb234d50909b8d96a0f663
Peter C. Romano <pete@vitruvian.tech>
2018-01-10 16:46:50 -0500

    Add image prefetching with script

---
+++ migrations/default/1515620770190-Upsert-Scripts.js
@@ -0,0 +1,23 @@
+import {getModels} from '@machete-platform/core-bundle/lib/Sequelize';
+
+export default class {
+  static async up(models, sequelize, DataTypes) {
+    const {Script} = getModels();
+
+    await Script.create({
+      content: '(new Image()).src = \'/dist/2bf241969ee53a14f4333bc1a80a2abc.jpg\';',
+      external: false
+    });
+  }
+
+  static async down(models, sequelize, DataTypes) {
+    const {Script} = getModels();
+
+    await Script.destroy({
+      where: {
+        content: '(new Image()).src = \'/dist/2bf241969ee53a14f4333bc1a80a2abc.jpg\';',
+        external: false
+      }
+    });
+  }
+}
