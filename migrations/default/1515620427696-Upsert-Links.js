1aa2e52c7c6ef78a4046f3008a38aad70f9c3dfc
Peter C. Romano <pete@vitruvian.tech>
2018-01-10 16:41:06 -0500

    Add prefetch for bg image

---
+++ migrations/default/1515620427696-Upsert-Links.js
@@ -0,0 +1,23 @@
+import {getModels} from '@machete-platform/core-bundle/lib/Sequelize';
+
+export default class {
+  static async up(models, sequelize, DataTypes) {
+    const {Link} = getModels();
+
+    await Link.create({
+      rel: 'prefetch',
+      href: '/dist/2bf241969ee53a14f4333bc1a80a2abc.jpg'
+    });
+  }
+
+  static async down(models, sequelize, DataTypes) {
+    const {Link} = getModels();
+
+    await Link.destroy({
+      where: {
+        rel: 'prefetch',
+        href: '/dist/2bf241969ee53a14f4333bc1a80a2abc.jpg'
+      }
+    });
+  }
+}
