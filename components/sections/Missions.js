3b7cbe627469cf72fa4a96f79e59d9147f13ea57
Peter C. Romano <pete@vitruvian.tech>
2018-02-27 19:51:36 -0500

    Our Work home screen

--- components/sections/Missions.js
+++ components/sections/Missions.js
@@ -5,10 +5,33 @@ export default class extends Section {
   render() {
     return (
       <Section>
-        <h1>Our Work</h1>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-        <p>Vestibulum pretium mi sem, id viverra nibh semper sit amet. Ut leo nisi, pulvinar at sapien at, vehicula accumsan metus. Maecenas vitae est ut nulla dictum gravida in nec purus. Nunc eget sodales augue, sit amet laoreet lectus. Nulla vitae elit elementum, fringilla nulla eu, lobortis ipsum. Sed laoreet, felis a mollis convallis, libero ex molestie libero, nec bibendum massa eros vitae tortor. In non ante at eros pharetra dapibus quis in mauris. Donec egestas tempus nulla, nec posuere lacus vulputate aliquam. Nam risus quam, dictum quis purus sit amet, ullamcorper dignissim enim. Quisque eu mollis diam. Aliquam et luctus orci, quis viverra elit.</p>
-        <p>Vestibulum pretium mi sem, id viverra nibh semper sit amet. Ut leo nisi, pulvinar at sapien at, vehicula accumsan metus. Maecenas vitae est ut nulla dictum gravida in nec purus. Nunc eget sodales augue, sit amet laoreet lectus. Nulla vitae elit elementum, fringilla nulla eu, lobortis ipsum. Sed laoreet, felis a mollis convallis, libero ex molestie libero, nec bibendum massa eros vitae tortor. In non ante at eros pharetra dapibus quis in mauris. Donec egestas tempus nulla, nec posuere lacus vulputate aliquam. Nam risus quam, dictum quis purus sit amet, ullamcorper dignissim enim. Quisque eu mollis diam. Aliquam et luctus orci, quis viverra elit.</p>
+      <h1>Our Work</h1>
+      <div className="background">
+        <div className="film" style={{ opacity: '.85' }}></div>
+        <div className="image" style={{ backgroundImage: 'url(/@vitruvian-tech/machete-bundle/images/insignia.png)', zIndex: '2', backgroundSize: '75%', backgroundPosition: '50% 65%' }}></div>
+        <div className="image" style={{ backgroundImage: 'url(/@vitruvian-tech/machete-bundle/images/home/missions-2.jpg)', opacity: '1' }}></div>
+        {/*<div className="image" style={{ backgroundImage: 'url(/@vitruvian-tech/machete-bundle/images/home/missions-1.jpg)', opacity: '.5' }}></div>*/}
+      </div>
+      <div className="row">
+        <div className="col-lg-4 col-xs-12">
+          <h2 className="text-center">The Mission</h2>
+          <p>Birthed from agency backgrounds and “front-end” (UI/UX) skill-sets, <em>VitruvianTech</em> puts an emphasis on design and the bottom line - which includes security and performance around everything we touch.</p>
+          <p>Built from our <em>Roman</em>-based principles, we believe design in any product must be minimal, absolutely functional, unconditionally  awless, and beautiful.</p>
+          <p><em>As the <strong>Digital Special Forces</strong>, we are the first line of defense in affecting your product’s digital presence.</em></p>
+        </div>
+        <div className="col-lg-4 col-xs-12 valign-middle">
+          <h2 className="text-center">Remote / Control</h2>
+          <p>Using state-of-the-art communication tools (Slack, Skype, etc.), conferencing services, and optimized project management tools, our consultants and developers are all remote-based autonomous experts, with  exibility for on-site engagements on a reasonable basis within the NYC tri-state area.</p>
+          <p>We have a proven track record of excellent project performance and price cuts by eliminating over- head with well-written requirements, constant com- munication from our staff, and  exible developer shifts, yielding a balanced work/life culture.</p>
+          <p><em>Communication is key, and we focus on deliverables and customer service, backed by our virtues.</em></p>
+        </div>
+        <div className="col-lg-4 col-xs-12">
+          <h2 className="text-center">The Process</h2>
+          <p><em>Foxtrot<sup>(sm)</sup></em> is our project management solution, based on Agile process methodologies.</p>
+          <p><strong>Three unique</strong> tracking systems (<em>Foxtrot One, Two,</em> and <em>Three</em>) lets you choose the process that best suits your projects’ needs - from quick ad-hoc projects, to large-scale product life-cycles.</p>
+          <p><em>Any of our three Foxtrot<sup>(sm)</sup> systems is included - free of charge - with our <strong>Managed Subscription</strong> plan!</em></p>
+        </div>
+      </div>
       </Section>
     );
   }
