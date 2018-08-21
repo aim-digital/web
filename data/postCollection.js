fadb1bd12968f6707a2599d0b225b55a09ed1b33
Peter C. Romano <pete@vitruvian.tech>
2018-08-21 18:12:27 -0400

    DSF marketing

--- data/postCollection.js
+++ data/postCollection.js
@@ -1,4 +1,4 @@
 export default {
     tagline: 'Digital Special Forces™ Disclosed Briefings & Tactics',
-    summary: 'Subscribe to VTTV and become your own DSF™ operative by learning about our tactics, Vitruvian Virtues, and methodologies.'
+    summary: 'Subscribe to VTTV and become your own Digital Special Forces™ operative by learning about our tactics, Vitruvian Virtues, and methodologies.'
   };
