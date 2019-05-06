7182d0f9ad49c6ebf7b086e8e4b5d933b7e3c397
Peter C. Romano <pete@vitruvian.tech>
2019-05-06 13:22:23 -0400

    Async/await for services

--- services/Solution.js
+++ services/Solution.js
@@ -1,12 +1,11 @@
-import async from '@machete-platform/core-bundle/lib/Promise';
 import {ContactSolution} from '@vitruvian-tech/machete-bundle/models/default';
 
-export const create = async(async (req, params, resolve, reject) => {
+export const create = async req => {
     const {email, solution} = req.body;
 
-    resolve(await ContactSolution.upsert({
+    return await ContactSolution.upsert({
       ContactEmail: email,
       SolutionId: solution.id,
       summary: solution.summary
-    }));
-});
+    });
+};
