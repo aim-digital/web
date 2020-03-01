e7dc3c022fe4529a5e174a5e878f606882559230
Peter C. Romano <pete@vitruvian.tech>
2020-03-01 17:07:43 -0500

    Add tagline to logo

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -4,21 +4,10 @@ export default class extends Component {
   render() {
     return (
       <div className="logo">
+        <div className="tagline">
+          <img src="/@fox-zero/web/images/tagline.png" />
+        </div>
         <img className="graphic" src="/@fox-zero/web/images/logo.png" />
-        {/*<div>
-          <img src="/@fox-zero/web/images/logo.png" />
-          <div className="corporate">
-            <div className="name">
-              <span>VitruvianTech</span>
-            </div>
-            <div className="tagline">
-              <span className="color-primary-blue">Roman</span>&nbsp;
-              <span className="color-primary-green">Inspired</span>&nbsp;
-              <span className="color-primary-yellow">Software</span>&nbsp;
-              <span className="color-secondary-red">Designers</span>
-            </div>
-          </div>
-        </div>*/}
       </div>
     );
   }
