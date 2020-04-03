469d9cd2250fe4bb7e3d43c9ef5c543229784e47
Peter C. Romano <pete@vitruvian.tech>
2020-04-03 15:15:23 -0400

    Update all meta tag data

--- migrations/default/1511981594494-Insert-Robots.js
+++ migrations/default/1511981594494-Insert-Robots.js
@@ -5,11 +5,6 @@ export default class {
     const {Robot} = getModels();
 
     await Robot.create({ rule: 'User-agent: *' });
-    await Robot.create({ rule: 'Disallow: /dashboard' });
-    await Robot.create({ rule: 'Disallow: /login' });
-    await Robot.create({ rule: 'Disallow: /chat' });
-    await Robot.create({ rule: 'Disallow: /survey' });
-    await Robot.create({ rule: 'Disallow: /widgets' });
     await Robot.create({ rule: 'Disallow: /home' });
     await Robot.create({ rule: 'Disallow: /home/' });
   }
@@ -18,11 +13,6 @@ export default class {
     const {Robot} = getModels();
 
     await Robot.destroy({ where: { rule: 'User-agent: *' } });
-    await Robot.destroy({ where: { rule: 'Disallow: /dashboard' } });
-    await Robot.destroy({ where: { rule: 'Disallow: /login' } });
-    await Robot.destroy({ where: { rule: 'Disallow: /chat' } });
-    await Robot.destroy({ where: { rule: 'Disallow: /survey' } });
-    await Robot.destroy({ where: { rule: 'Disallow: /widgets' } });
     await Robot.destroy({ where: { rule: 'Disallow: /home' } });
     await Robot.destroy({ where: { rule: 'Disallow: /home/' } });
   }
